854c90db19ac4670675111286efb2b8256eddcb9
Krxwallo <ken.grill@gmx.de>
2021-02-23 09:04:37 +0100

    Fixed bot not working

--- bigbluebot_win.bat
+++ bigbluebot_win.bat
@@ -1,2 +1,2 @@
 @echo off
-cmd /k "cd /d C:\Users\Ken\Documents\discordbots\venv\Scripts & activate & cd /d    C:\Users\Ken\Documents\discordbots & python -u bigbluebot.py"
+cmd /k "cd /d C:\Users\Ken\Documents\discordbots\venv\Scripts & activate & cd /d    C:\Users\Ken\Documents\discordbots & python -u bigbluebot.py >> logs\\latest.log"
